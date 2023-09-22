9299dbc3a99c6671dd365781addac4afb26948cc
Ivan Templora <ivantemplora@gmail.com>
2023-09-22 09:40:04 -0600

    Finished Assignment 3 & Added CSS Styling

--- app/page.js
+++ app/page.js
@@ -4,9 +4,12 @@ import StudentInfo from "./student-info"
 export default function Home() {
   return (
     <main>
-      <h1>CPRG 306: Web Development 2 - Assignments</h1>
+      <h1 className="text-4xl font-bold text-yellow-500">CPRG 306: Web Development 2 - Assignments</h1><br />
       <StudentInfo />
-      <Link href="week2">Week 2</Link>
+      <div className="m-4 font-bold">
+        <Link href="week2">Week 2</Link><br />
+        <Link href="week3">Week 3</Link>
+      </div>
     </main>
   )
 }
--- app/student-info.js
+++ app/student-info.js
@@ -2,10 +2,10 @@ import Link from "next/link"
 
 export default function StudentInfo() {
     return (
-        <main>
-            <p>Name: Ivan Templora</p>
-            <p>Course section: CPRG 306 C</p>
+        <div className="m-4 bg-green-700 w-max border-2 border-emerald-600">
+            <p className="font-bold">Name: Ivan Templora</p>
+            <p className="font-bold">Course section: CPRG 306 C</p>
             <Link href="https://github.com/IvanTemplora?tab=repositories">https://github.com/IvanTemplora?tab=repositories</Link>
-        </main>
+        </div>
     )
 }
